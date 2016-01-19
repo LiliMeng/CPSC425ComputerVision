8a76d43b9aac77fdee376d539f0ff6b769a17e96
LiliMeng <lilimeng1103@gmail.com>
2016-01-18 23:08:28 -0800

    Update Gaussian1d.py

--- Gaussian1d.py
+++ Gaussian1d.py
@@ -6,22 +6,25 @@ from scipy import signal
 def gauss1d(sigma):
     tmp=round(sigma*6)
     if tmp%2!=0:
-    	newSigma=tmp
+    	length=tmp
     else:
-    	newSigma=tmp+1
+    	length=tmp+1
     #print "the new sgima value is %f:" %newSigma
+    x=range(-(int)(length/2),(int)(length/2+1))
     PI=3.1415926
-    x=[-3, -2, -1, 0, 1, 2, 3]
+    
     results=[]
     print "the result for sigma value %f is the following:" %sigma
     for index in range(len(x)):
-    	result= 1/(math.sqrt(2*PI)*math.pow(newSigma, 2))*math.exp(-math.pow(x[index],2)/(2*math.pow(newSigma,2))) 
+    	result= 1/(math.sqrt(2*PI)*math.pow(sigma, 2))*math.exp(-math.pow(x[index],2)/(2*math.pow(sigma,2))) 
     	results.append(result)
-    print results
-    return 
+    total=sum(results)
+    results[:] = [x / total for x in results]
+    #print results
+    return np.array(results)
 
 
-gauss1d(0.3)
-gauss1d(0.5)
-gauss1d(1)
-gauss1d(2)
+print gauss1d(0.3)
+print gauss1d(0.5)
+print gauss1d(1)
+print gauss1d(2)
