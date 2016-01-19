21bab1d6ac7b31e253e9ae74c21000889d66d303
LiliMeng <lilimeng1103@gmail.com>
2016-01-19 09:31:10 -0800

    Update Gaussian2d.py

--- Gaussian2d.py
+++ Gaussian2d.py
@@ -10,8 +10,7 @@ def gauss1d(sigma):
     else:
     	length=tmp+1
     x=range(-(int)(length/2),(int)(length/2+1))
-    PI=3.1415926
-    
+    PI=3.1415926 
     results=[]
     print "the result for sigma value %f is the following:" %sigma
     for index in range(len(x)):
@@ -22,12 +21,12 @@ def gauss1d(sigma):
     #print results
     return np.array(results)
 
-def guass2d(sigma):
-    guas1d=gauss1d(sigma)
-    guas1d = guas1d[np.newaxis]  #To convert a 1D array, f, to a 2D array f, of the same size you use ‘f = f[np.newaxis]’
-    guas1dTrans=np.transpose(guas1d)
-    finalResult=signal.convolve2d(guas1d,guas1dTrans) # a 2D Gaussian can be formed by convolution of a 1D Gaussian with its transpose.
+def gauss2d(sigma):
+    gaus1d=gauss1d(sigma)
+    gaus1d = gaus1d[np.newaxis]
+    gaus1dTrans=np.transpose(gaus1d)
+    finalResult=signal.convolve2d(gaus1d,gaus1dTrans)  # a 2D Gaussian can be formed by convolution of a 1D Gaussian with its transpose.
     return finalResult
 
-print guass2d(0.5)
-print guass2d(1)
+print gauss2d(0.5)
+print gauss2d(1)
