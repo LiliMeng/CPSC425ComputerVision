22f5a71b256948fe0c0e5f374bd86d6baa14a1ab
LiliMeng <lilimeng1103@gmail.com>
2016-02-02 10:43:48 -0800

    Update assign3.py

--- assign3.py
+++ assign3.py
@@ -40,45 +40,52 @@ def FindTemplate(pyramid, template, threshold):
 
     finalWidth= 15
     downsampleScale=template.size[0]/finalWidth 
-    template = template.resize((finalWidth, template.size[1]/downsampleScale), Image.BICUBIC)
+    finalHeight=template.size[1]/downsampleScale
+    template = template.resize((finalWidth, finalHeight), Image.BICUBIC)
+    #print template.size 
     
-    points=[]
+    img = pyramid[0].convert('RGB')
     
+    print 'the length of the pyramid'
+    print len(pyramid) 
     for image in pyramid:
-	normCrossCorr=ncc.normxcorr2D(image, template)
-        largerThresh=np.where(normCrossCorr > threshold)
-        points.append(zip(largerThresh[1], largerThresh[0]))
- 
-        img= pyramid[0].convert('RGB')
-        
-        for i in range(len(points)):
-		point=points[i]
-                scale=0.75*i
-                print scale
-
-                for pt in point:
-                    ptx=pt[0]/scale
-                    pty=pt[1]/scale
-
-                    ptx1=template.size[0]/(2*scale)
-                    pty1=template.size[1]/(2*scale)
-
-                    x1 = ptx-ptx1
-                    y1 = pty-pty1
-                    x2 = ptx+ptx1
-                    y2 = pty+pty1
-                    draw=ImageDraw.Draw(img)
-                    draw.line((x1,y1,x2,y2),fill="red",width=2)
-    		    del draw
+	NCC=ncc.normxcorr2D(image, template)
+	print 'NCC value'
+	print NCC
+        aboveThresh=np.where(NCC > threshold)
+        print 'the points above threshold'
+        print aboveThresh
+        xlist=aboveThresh[1]
+        ylist=aboveThresh[0]
+        print xlist
+        print ylist
+        ptx1=template.size[0]/2
+        pty1=template.size[1]/2
+        print len(xlist)
+        for i in range(len(xlist)):
+			x1=xlist[i]-ptx1
+			y1=ylist[i]-pty1
+			x2=xlist[i]+ptx1
+			y2=ylist[i]+pty1
+			print x1,x2,y1,y2
+			draw=ImageDraw.Draw(img)
+			draw.rectangle((x1,y1,x2,y2),outline="red")
+			#del draw
+	img.save('result.png','PNG')
+	
 	return img
 
 
 def run():
     
-	images=['faces/judybats.jpg','faces/students.jpg','/faces/tree.jpg']
+	#images=['faces/judybats.jpg','faces/students.jpg','faces/tree.jpg']
+	#images=['faces/judybats.jpg']
+	images=['faces/students.jpg']
+	#images=['faces/tree.jpg']
 	for image in images:
 		img = Image.open(image)
         	img = img.convert('L')
+    
 
         	pyramid = MakePyramid(img,20)
         	ShowPyramid(pyramid)
@@ -89,9 +96,8 @@ def run():
 
       
 
-       	 	threshold =0.532
+       	 	threshold =0.51
         	result =FindTemplate(pyramid,template, threshold)
-        	result.save('result.png','PNG')
         	result.show()
     	return
 
