1c4ac751418e4542d579e014fa53d2328936d6bd
LiliMeng <lilimeng1103@gmail.com>
2016-03-11 11:58:23 -0800

    Update boxfilter.py

--- boxfilter.py
+++ boxfilter.py
@@ -1,18 +1,15 @@
-from PIL import Image
+
 import numpy as np
-import math
-from scipy import signal
+
 
 def boxfilter(n):
     assert n%2!=0, "The Dimensions must be odd"
     b=np.full((n,n),0.04)
-    total=sum(map(sum, b))
-    b[:] = [x / total for x in b]
-    print b
-    return b
-
-boxfilter(3)
-boxfilter(4)
-boxfilter(5)
-
+    total=np.sum(np.sum(b))
+    result = b/total
+    return result
+    
+print boxfilter(3)
+print boxfilter(5)
+print boxfilter(4)
 
